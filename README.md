a21b481071295f181381e5b9ac7262eab3cebf09
Hugh Brown <hughdbrown@yahoo.com>
2023-08-10 15:11:10 -0600

    Yuck.
    Added code from another repo to implement benchmarking.

--- README.md
+++ README.md
@@ -26,3 +26,20 @@ test tests::factors_44711100255155897 ... ok
 test result: ok. 16 passed; 0 failed; 0 ignored; 0 measured; 0 filtered out; finished in 0.68s
 ```
 
+# main
+The main entry point tests the raw factorization code against code that starts with a vector of primes.
+The course text suggests this optimization is worthwhile, at least when the amortized cost of creation
+of the primes is considered.
+
+Here are the results for this test case when using a release build:
+```
+Build sieve: 439.330042ms seconds
+
+find_factors: 67.381086ms seconds
+[45269999, 987654103]
+Product: 44711100255155897
+
+find_factors_sieve: 24.847313ms seconds
+[45269999, 987654103]
+Product: 44711100255155897
+```
---
+++ src/main.rs
@@ -0,0 +1,81 @@
+use std::time::{Instant};
+
+use prime_factors::{
+    find_factors,
+    find_factors_sieve,
+    multiply_vector,
+};
+
+// Build a sieve of Eratosthenes.
+pub fn sieve_of_eratosthenes(max: usize) -> Vec<bool> {
+    if max == 0 {return vec![false]};
+    if max == 1 {return vec![false, false]};
+
+    let mut values = vec![true; max + 1];
+    values[0] = false;
+    values[1] = false;
+    let upper: usize = ((max as f64).sqrt() as usize) + 1;
+    for i in 2..upper {
+        if values[i] {
+            // println!("-----\nSweeping {i}");
+            for j in ((i*i)..=max).step_by(i) {
+                // println!("Marking {j}");
+                values[j] = false;
+            }
+        }
+    }
+    values
+}
+
+pub fn sieve_to_primes(sieve: &[bool]) -> Vec<usize> {
+    sieve.iter()
+        .enumerate()
+        .filter(|pair| *pair.1) // Include pair if prime flag is true
+        .map(|pair| pair.0) // Change type by passing on only the usize position/prime-number
+        .collect::<Vec::<usize>>()
+}
+
+
+// We'll use primes up to this value.
+//const MAX_PRIME: usize = 10_000_000;
+const MAX_PRIME: usize = 50_000_000;
+
+fn main() {
+    // Build a vector of primes.
+    let start0 = Instant::now();
+    let sieve = sieve_of_eratosthenes(MAX_PRIME);
+    let primes = sieve_to_primes(&sieve);
+    let duration0 = start0.elapsed();
+    println!("Build sieve: {:?} seconds\n", duration0);
+
+    // Multiply two big primes so we have something interesting to factor.
+    //let big_number = 987654103_i64 * 45269999_i64;
+    //println!("{} * {} = {}",  987654103_i64, 45269999_i64, big_number);
+
+    // loop {
+    {
+        // Get the number to factor.
+        //let num = get_i64("Number to factor: ");
+        let num = 987654103_usize * 45269999_usize;
+
+        // Find the factors the slow way.
+        let start1 = Instant::now();
+        let factors1 = find_factors(num);
+        let duration1 = start1.elapsed();
+        println!("find_factors: {:?} seconds", duration1);
+        //print_numbers(&mut factors1);
+        println!("{:?}", factors1);
+        println!("Product: {}", multiply_vector(&factors1));
+        println!();
+
+        // Use the Euler's sieve to find the factors.
+        let start2 = Instant::now();
+        let factors2 = find_factors_sieve(num, &primes);
+        let duration2 = start2.elapsed();
+        println!("find_factors_sieve: {:?} seconds", duration2);
+        println!("{:?}", factors2);
+        println!("Product: {}", multiply_vector(&factors2));
+        println!();
+    }
+}
+
